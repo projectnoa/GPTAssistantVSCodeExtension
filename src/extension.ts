6ac75d6b22cd299b93b5d31bdd0b290d6bc96ea3
upx <juanmanuel.reyes@gmail.com>
2023-04-06 15:32:24 -0700

    Fixes

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,49 @@
 import { Event, EventEmitter, ExtensionContext, ProgressLocation, TreeDataProvider, TreeItem, commands, window, workspace } from 'vscode';
 import axios from 'axios';
 
+const extensionName = 'juandy-gpt-assistant';
+
+const optimizeCommand = `${extensionName}.optimize`;
+const documentCommand = `${extensionName}.document`;
+const analyzeCommand = `${extensionName}.analyze`;
+const dryCommand = `${extensionName}.dry`;
+const inquireCommand = `${extensionName}.inquire`;
+
+const apiKeyKey = 'apiKey';
+const optimizePromptKey = 'optimizePrompt';
+const documentPromptKey = 'documentPrompt';
+const analyzePromptKey = 'analyzePrompt';
+const dryPromptKey = 'dryPrompt';
+
+const chatAPIURL = 'https://api.openai.com/v1/chat/completions';
+const editAPIURL = 'https://api.openai.com/v1/edits';
+
+const chatModel = 'gpt-3.5-turbo';
+const editModel = 'code-davinci-edit-001';
+
+const apikeyMessage = 'Please provide your OpenAI API key in the extension settings.';
+const missingAPIKeyMessage = 'Missing OpenAI API key. Please provide your OpenAI API key in the extension settings.';
+
+const chatPrompt = 'You are a helpful coding assistant.';
+
+const defaultError = 'An error occurred. Please try again later.';
+const noTextSelected = 'No text selected.';
+const noInput = 'No input provided.';
+
+const languageKey = '{{LANG}}';
+
+const optimizePrompt = `You are a code optimizer that receives ${languageKey} code and outputs an optimized version of the ${languageKey} code.`;
+const documentPrompt = `You are a code documenting tool that receives ${languageKey} code and outputs the same code with comments in each line.`;
+const analyzePrompt = `You are a code analyzer that receives ${languageKey} code and outputs an brief explanation of what the code does in plain English`;
+const dryPrompt = `You are a code optimizer that receives ${languageKey} code and outputs refactored, concise, and DRY ${languageKey} code.`;
+const inquirePrompt = 'Ask a question about this code.';
+
+const onlyCodeDirective = ' Only reply with the output inside one unique code block, and nothing else. Do not write explanations.';
+
+const resourseIcon = 'resources/icon.ico';
+
+const panelViewId = 'prompt-panel-view';
+
 /**
  * This method is called when the extension is activated.
  * It registers the commands and adds them to the context subscriptions.
@@ -10,12 +53,6 @@ import axios from 'axios';
  * @param context Context of the extension
  */
 export function activate(context: ExtensionContext) {
-	const optimizePrompt = 'You are a code optimizer that receives {{LANG}} code and outputs an optimized version of the {{LANG}} code.';
-	const documentPrompt = 'You are a code documenting tool that receives {{LANG}} code and outputs the same code with comments in each line.';
-	const analyzePrompt = 'You are a code analyzer that receives {{LANG}} code and outputs an brief explanation of what the code does in plain English';
-	const dryPrompt = 'You are a code optimizer that receives {{LANG}} code and outputs refactored, concise, and DRY {{LANG}} code.';
-
-	const onlyCodeDirective = ' Only reply with the output inside one unique code block, and nothing else. Do not write explanations.';
 	
 	const progressOptions = {
 		location: ProgressLocation.Notification,
@@ -24,14 +61,14 @@ export function activate(context: ExtensionContext) {
 	};
 
 	// Register the optimize command and add it to the context subscriptions
-	const optimizeDisposable = commands.registerCommand('juandy-gpt-assistant.optimize', async () => {
+	const optimizeDisposable = commands.registerCommand(optimizeCommand, async () => {
 		await window.withProgress(
 			progressOptions,
 			() =>
 				executeCommand(
 					workspace
-						.getConfiguration('juandy-gpt-assistant')
-						.get('optimizePrompt', optimizePrompt) + onlyCodeDirective)
+						.getConfiguration(extensionName)
+						.get(optimizePromptKey, optimizePrompt) + onlyCodeDirective)
 		  );
 		}
 	);
@@ -39,14 +76,14 @@ export function activate(context: ExtensionContext) {
 	context.subscriptions.push(optimizeDisposable);
 	
 	// Register the document command and add it to the context subscriptions
-	const documentDisposable = commands.registerCommand('juandy-gpt-assistant.document',  async () => {
+	const documentDisposable = commands.registerCommand(documentCommand,  async () => {
 		await window.withProgress(
 			progressOptions,
 			() =>
 				executeCommand(
 					workspace
-						.getConfiguration('juandy-gpt-assistant')
-						.get('documentPrompt', documentPrompt) + onlyCodeDirective)
+						.getConfiguration(extensionName)
+						.get(documentPromptKey, documentPrompt) + onlyCodeDirective)
 		  );
 		}
 	);
@@ -54,14 +91,14 @@ export function activate(context: ExtensionContext) {
 	context.subscriptions.push(documentDisposable);
 
 	// Register the analyze command and add it to the context subscriptions
-	const analizeDisposable = commands.registerCommand('juandy-gpt-assistant.analyze', async () => {
+	const analizeDisposable = commands.registerCommand(analyzeCommand, async () => {
 		await window.withProgress(
 			progressOptions,
 			() =>
 				executeCommand(
 					workspace
-						.getConfiguration('juandy-gpt-assistant')
-						.get('analyzePrompt', analyzePrompt), false)
+						.getConfiguration(extensionName)
+						.get(analyzePromptKey, analyzePrompt), false)
 		  );
 		}
 	);
@@ -69,14 +106,14 @@ export function activate(context: ExtensionContext) {
 	context.subscriptions.push(analizeDisposable);
 
 	// Register the DRY command and add it to the context subscriptions
-	const dryDisposable = commands.registerCommand('juandy-gpt-assistant.dry', async () => {
+	const dryDisposable = commands.registerCommand(dryCommand, async () => {
 		await window.withProgress(
 			progressOptions,
 			() =>
 				executeCommand(
 					workspace
-						.getConfiguration('juandy-gpt-assistant')
-						.get('dryPrompt', dryPrompt) + onlyCodeDirective)
+						.getConfiguration(extensionName)
+						.get(dryPromptKey, dryPrompt) + onlyCodeDirective)
 		  );
 		}
 	);
@@ -84,7 +121,7 @@ export function activate(context: ExtensionContext) {
 	context.subscriptions.push(dryDisposable);
 
 	// Register the inquire command and add it to the context subscriptions
-	const inquireDisposable = commands.registerCommand('juandy-gpt-assistant.inquire', async () => {
+	const inquireDisposable = commands.registerCommand(inquireCommand, async () => {
 		await window.withProgress(
 			progressOptions,
 			async () => {
@@ -102,24 +139,23 @@ export function activate(context: ExtensionContext) {
 
 				// If there is no text selected, show an information message and exit the function
 				if (!selectedText) {
-					window.showInformationMessage('No text selected.');
+					window.showInformationMessage(noTextSelected);
 
 					return;
 				}
 
 				// Display an input box and request user input
 				const userInput = await window.showInputBox({
-					prompt: 'Ask a question about this code.',
-					value: selectedText
+					prompt: inquirePrompt
 				});
 
 				// Check if the user provided input or dismissed the input box
 				if (userInput !== undefined) {
 					// Handle the user input
-					return executeCommand(userInput, false);
+					return executeCommand(userInput + selectedText, false);
 				} else {
 					// The user dismissed the input box without providing input
-					window.showWarningMessage('No input provided');
+					window.showWarningMessage(noInput);
 				}
 			}
 		  );
@@ -129,7 +165,7 @@ export function activate(context: ExtensionContext) {
 	context.subscriptions.push(inquireDisposable);
 
 	// Register the panel view and add it to the context subscriptions
-	const panelView = window.createTreeView('prompt-panel-view', {
+	const panelView = window.createTreeView(panelViewId, {
 		treeDataProvider: new PanelViewDataProvider(context),
 		showCollapseAll: true
 	});
@@ -152,29 +188,29 @@ class PanelViewDataProvider implements TreeDataProvider<TreeItem> {
   
 	getChildren(element?: TreeItem): Thenable<TreeItem[]> {
 		const iconPath = {
-			light: this.context.asAbsolutePath('resources/icon.ico'),
-			dark: this.context.asAbsolutePath('resources/icon.ico')
+			light: this.context.asAbsolutePath(resourseIcon),
+			dark: this.context.asAbsolutePath(resourseIcon)
 		};
 
 		if (element === undefined) {
 			const optimizeButton = new TreeItem('Optimize');
-			optimizeButton.command = { command: 'juandy-gpt-assistant.optimize', title: 'Optimize', arguments: [] };
+			optimizeButton.command = { command: optimizeCommand, title: 'Optimize', arguments: [] };
 			optimizeButton.iconPath = iconPath;
 
 			const documentButton = new TreeItem('Document');
-			documentButton.command = { command: 'juandy-gpt-assistant.document', title: 'Document', arguments: [] };
+			documentButton.command = { command: documentCommand, title: 'Document', arguments: [] };
 			documentButton.iconPath = iconPath;
 
 			const analyzeButton = new TreeItem('Analyze');
-			analyzeButton.command = { command: 'juandy-gpt-assistant.analyze', title: 'Analyze', arguments: [] };
+			analyzeButton.command = { command: analyzeCommand, title: 'Analyze', arguments: [] };
 			analyzeButton.iconPath = iconPath;
 
 			const dryButton = new TreeItem('DRY');
-			dryButton.command = { command: 'juandy-gpt-assistant.dry', title: 'DRY', arguments: [] };
+			dryButton.command = { command: dryCommand, title: 'DRY', arguments: [] };
 			dryButton.iconPath = iconPath;
 
 			const inquireButton = new TreeItem('Inquire');
-			inquireButton.command = { command: 'juandy-gpt-assistant.inquire', title: 'Inquire', arguments: [] };
+			inquireButton.command = { command: inquireCommand, title: 'Inquire', arguments: [] };
 			inquireButton.iconPath = iconPath;
 			
 			return Promise.resolve([optimizeButton, documentButton, analyzeButton, dryButton, inquireButton]);
@@ -208,7 +244,7 @@ async function executeCommand(instruction: string, edit: boolean = true) {
 
 	// If there is no text selected, show an information message and exit the function
 	if (!selectedText) {
-		window.showInformationMessage('No text selected.');
+		window.showInformationMessage(noTextSelected);
 
 		return;
 	}
@@ -217,7 +253,7 @@ async function executeCommand(instruction: string, edit: boolean = true) {
 	try {
 		if (edit) {
 			// Replace the {{CODE}} placeholder with the language ID
-			const prompt = instruction.replace('{{LANG}}', languageId);
+			const prompt = instruction.replace(languageKey, languageId);
 			// Display an information popup
 			//console.log(`PROMPT: \n${prompt}\n\n`);
 			// Generate the edited code
@@ -251,36 +287,29 @@ async function executeCommand(instruction: string, edit: boolean = true) {
  * @throws An error if the API response is invalid or if the request fails
  */
 async function generateEdit(prompt: string, instruction: string): Promise<string> {
-	const apiKey = workspace.getConfiguration('juandy-gpt-assistant').get('apiKey');
-	const apiURL = 'https://api.openai.com/v1/edits';
+	const apiKey = workspace.getConfiguration(extensionName).get(apiKeyKey);
+	const apiURL = editAPIURL;
 
 	// Check if the API key is provided
 	if (!apiKey) {
-		window.showErrorMessage('Please configure the API key in the settings.');
+		window.showErrorMessage(missingAPIKeyMessage);
 		
-		throw new Error('API key not configured');
+		throw new Error(missingAPIKeyMessage);
 	}
 
 	const data = {
-		model: "code-davinci-edit-001",
+		model: editModel,
 		input: prompt,
 		instruction: instruction,
 	};
 
 	try {
-		const response = 
-			await axios.post(apiURL, data, {
-				headers: {
-					'Content-Type': 'application/json',
-					'Authorization': `Bearer ${apiKey}`,
-				},
-      			timeout: 30000, // Timeout in milliseconds (10 seconds)
-			});
+		const response = await makeRequest(apiURL, data, apiKey);
 
 		if (response.data.choices?.length > 0) {
 			return response.data.choices[0].text.trim();
 		} else {
-			throw new Error('No completion generated');
+			throw new Error(defaultError);
 		}
 	} catch (error: any) {
 		throw error;
@@ -296,22 +325,22 @@ async function generateEdit(prompt: string, instruction: string): Promise<string
  * @throws An error if the API response is invalid or if the request fails
  */
 async function generateCompletion(prompt: string): Promise<string> {
-	const apiKey = workspace.getConfiguration('juandy-gpt-assistant').get('apiKey');
-	const apiURL = 'https://api.openai.com/v1/chat/completions';
+	const apiKey = workspace.getConfiguration(extensionName).get(apiKeyKey);
+	const apiURL = chatAPIURL;
 
 	// Check if the API key is provided
 	if (!apiKey) {
-		window.showErrorMessage('Please configure the API key in the settings.');
+		window.showErrorMessage(missingAPIKeyMessage);
 
-		throw new Error('API key not configured');
+		throw new Error(missingAPIKeyMessage);
 	}
 
 	const data = {
-		model: "gpt-3.5-turbo",
+		model: chatModel,
 		messages: [
 			{
 				"role": "system",
-				"content": "You are a helpful coding assistant."
+				"content": chatPrompt
 			},
 			{
 				"role": "user",
@@ -321,20 +350,29 @@ async function generateCompletion(prompt: string): Promise<string> {
 	};
 
 	try {
-		const response = 
-			await axios.post(apiURL, data, {
-				headers: {
-					'Content-Type': 'application/json',
-					'Authorization': `Bearer ${apiKey}`,
-				},
-      			timeout: 30000, // Timeout in milliseconds (10 seconds)
-			});
+		const response = await makeRequest(apiURL, data, apiKey);
 
 		if (response.data.choices?.length > 0) {
 			return response.data.choices[0].message.content.trim();
 		} else {
-			throw new Error('No completion generated');
+			throw new Error(defaultError);
 		}
+	} catch (error: any) {
+		throw error;
+	}
+}
+
+async function makeRequest(url: string, data: any, apiKey: any, timeout: number = 30000): Promise<any> {
+	try {
+		const response = 
+			await axios.post(url, data, {
+				headers: {
+					'Content-Type': 'application/json',
+					'Authorization': `Bearer ${apiKey}`,
+				},
+				timeout: timeout,
+			});
+		return response;
 	} catch (error: any) {
 		throw error;
 	}
